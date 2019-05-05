c253b502681e10e470939981adccaa4c07daf055
sun-23 <twinsamson@gmail.com>
2019-05-05 13:58:28 +0700

    ok

--- CoreMLResent50/ViewController.swift
+++ CoreMLResent50/ViewController.swift
@@ -7,14 +7,125 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Answer: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
     }
+    
+    
+    @IBAction func banana(_ sender: UIButton) {
+        
+        coreMLResNet50(ImgName: "banana")
+    }
+    
+    
+    @IBAction func orange(_ sender: UIButton) {
+        
+        coreMLResNet50(ImgName: "orange")
+    }
+    
 
-
+    @IBAction func apple(_ sender: UIButton) {
+        
+        coreMLResNet50(ImgName: "dog")
+    }
+    
+    
+    @IBAction func car(_ sender: UIButton) {
+        
+        coreMLResNet50(ImgName: "cat")
+    }
+    
+    
+    @IBAction func boat(_ sender: UIButton) {
+        
+        coreMLResNet50(ImgName: "hamster")
+    }
+    
+    
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+    
+    func coreMLResNet50(ImgName:String)  {
+        
+        for v in view.subviews{
+            if v is UIImageView {
+                v.removeFromSuperview()   // ล้างภาพ
+            }
+        }
+        
+        self.Answer.text = ""
+        
+       let image = UIImage(named: ImgName)
+        
+        imageView.image =  image
+        
+        let sceledHeight = view.frame.width / image!.size.width * image!.size.height
+        let imageScaledHeight = view.frame.size.width / image!.size.width * image!.size.height
+        
+        // ให้ภาพอยู่จุดกึ่งกลางของ แกน y
+        imageView.frame = CGRect(x: 0, y: view.frame.height - ( view.frame.height / 2 + sceledHeight / 2 ), width: view.frame.width, height: sceledHeight)
+        
+        imageView.backgroundColor = .black
+        
+        view.backgroundColor = .black
+        
+        
+        view.addSubview(imageView)
+        
+        let resnetModel = Resnet50()
+        
+        // เอา model ใส่ใน VNCoreMLModel /-------------------/
+        if let model = try? VNCoreMLModel(for: resnetModel.model) {
+            
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                
+                if let result = request.results as? [VNClassificationObservation] {
+                    
+                    for classification in result where classification.confidence > 0.5 {
+                        
+                        print("ID : \(classification.identifier), confidence: \(classification.confidence)")
+                        self.Answer.text! = "ID : \(classification.identifier) , Confidence: \(Int((classification.confidence) * 100)) %"
+                        
+                    }
+                    
+                }
+            }
+            
+            // Handler request above
+            
+            if let image = UIImage(named: ImgName){
+                
+                if let imageData = image.pngData() {
+                    let handler = VNImageRequestHandler(data: imageData, options: [:])
+                    try? handler.perform([request])
+                    
+                }
+                
+            }
+            
+            
+            
+        }
+        // /-----------------------------------------------------/
+        
+    }
+    
+    
 }
 
